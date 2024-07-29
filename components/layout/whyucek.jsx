a1a0ddbb4d2748f3fa71938dfa1ea09fadadbeaf
Richard Shaju <richardshaju66@gmail.com>
2024-07-30 03:01:08 +0530

    Announcement cards added

--- components/layout/whyucek.jsx
+++ components/layout/whyucek.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import whybg from '@/public/img/whybg.png';
+import whybg from "@/public/img/whybg.png";
 
 const WhyUcek = () => {
   return (
@@ -9,8 +9,26 @@ const WhyUcek = () => {
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-white text-center p-4">
         <h2 className="text-3xl md:text-4xl font-bold mb-4">Why UCEK?</h2>
-        <p className="mb-8 max-w-2xl text-sm md:text-base lg:text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+        <p class="text-white text-sm md:text-xl  font-sans md:px-44 md:py-6">
+          University College of Engineering Kariavattom (UCEK), the pride of the
+          Kariavattom campus spread across
+          <strong> 400 acres </strong>
+          UCEK, the center of innovation, neighbors the IT hub{" "}
+          <strong>Technopark</strong>, and<strong> UST Global</strong>, and
+          features the <strong>Greenfield International Stadium </strong>just a
+          peek away, state-of-the-art labs, and seasoned faculty.{" "}
+          <strong>
+            {" "}
+            UCEK stands as a symbol of academic heritage and eminence.{" "}
+          </strong>{" "}
+          What sets us apart is our success in holistic development,
+          demonstrated by our alumni network and notable{" "}
+          <strong> National </strong>and{" "}
+          <strong>International Communities</strong> such as{" "}
+          <strong>IEEE, NSS,</strong>and <strong>FOSS CELL. </strong> Other
+          communities such as <strong> IEDC, GDSC, Mulearn </strong>and
+          <strong> TinkerHub </strong>have made significant contributions to
+          what makes UCEK proud.
         </p>
         <div className="flex flex-wrap justify-around w-full max-w-4xl gap-4">
           <div className="text-center flex-1 min-w-[120px]">
